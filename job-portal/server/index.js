928f8ef9e7f9243c1ee1a082c18cea679baf3eb0
saloni kumari <salonikumari005dnr@gmail.com>
2026-05-06 12:51:47 +0530

    Final CORS and Port Fix

--- job-portal/server/index.js
+++ job-portal/server/index.js
@@ -39,9 +39,9 @@ connectDB();
 // --- MIDDLEWARES ---
 // --- MIDDLEWARES ---
 app.use(cors({
-    origin: "https://job-portal-project-1-7llf.onrender.com", // Sirf ye ek link rakhein jo console mein dikh raha hai
+    origin: "https://job-portal-project-1-7llf.onrender.com", 
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // OPTIONS zaroori hai pre-flight ke liye
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"]
 }));
 
